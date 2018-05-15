5cecaa03408df51223b83adb675a3cdf4944622d
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-15 09:07:10 -0700

    eroienr

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/restaurants/:id', (req, res) => {
       <link rel="stylesheet" href="http://13.57.205.164:3004/restaurants/styles.css">
     </head>
     <body>
+    <script>import ReactDOM from 'react-dom'</script>
       <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
       <div id="app"></div>
