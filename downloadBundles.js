69b88125a631723605ba0edfde5fcbd80534fda3
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-14 17:02:24 -0700

    wefqwef

--- downloadBundles.js
+++ downloadBundles.js
@@ -2,6 +2,6 @@ const http = require('http');
 const fs = require('fs');
 
 const file = fs.createWriteStream('./public/Nearby/bundle.js');
-http.get('http://localhost:3004/restaurants/bundle.js', (response) => {
+http.get('http://13.57.205.164:3004/restaurants/bundle.js', (response) => {
   response.pipe(file);
 });
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/restaurants/:id', (req, res) => {
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css" rel="stylesheet">
     <title>Zaget</title>
-    <link rel="stylesheet" href="http://localhost:3004/restaurants/styles.css">
+    <link rel="stylesheet" href="http://13.57.205.164:3004/restaurants/styles.css">
   </head>
   <body>
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
@@ -47,7 +47,7 @@ app.get('/restaurants/:id', (req, res) => {
       <div id="apateezSidebar"></div>
     </div>
     <div id="nearby-app">${markup}</div>
-    <script src="http://localhost:3004/restaurants/bundle.js"></script>
+    <script src="http://13.57.205.164:3004/restaurants/bundle.js"></script>
   </body>
 </html>
 
