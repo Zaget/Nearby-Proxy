aac4c308d6f79213131d245c757eac13ef430ed3
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-18 20:10:51 -0700

    weifwenf

--- downloadBundles.js
+++ downloadBundles.js
@@ -6,7 +6,4 @@ http.get('http://nearby-414751775.us-west-1.elb.amazonaws.com:3004/restaurants/b
   response.pipe(file);
 });
 
-const file = fs.createWriteStream('./public/Sidebar/bundle.js');
-http.get('http://nearby-414751775.us-west-1.elb.amazonaws.com:3004/restaurants/bundle.js', (response) => {
-  response.pipe(file);
-});
+
