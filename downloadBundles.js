17d830d52400b805300f474988bb8762282a0222
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-05-18 19:28:19 -0700

    weiwenbf

--- downloadBundles.js
+++ downloadBundles.js
@@ -2,6 +2,6 @@ const http = require('http');
 const fs = require('fs');
 
 const file = fs.createWriteStream('./public/Nearby/bundle.js');
-http.get('http://nearby-414751775.us-west-1.elb.amazonaws.com:3004/restaurants/styles.css', (response) => {
+http.get('http://nearby-414751775.us-west-1.elb.amazonaws.com:3004/restaurants/bundle.js', (response) => {
   response.pipe(file);
 });
